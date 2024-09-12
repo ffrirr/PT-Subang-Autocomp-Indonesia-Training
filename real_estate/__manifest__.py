a6305b7ba9f71bce9b4a362aa3225aa743f0257e
ffrirr <frrwn98@gmail.com>
2024-09-12 16:08:12 +0700

    update

--- real_estate/__manifest__.py
+++ real_estate/__manifest__.py
@@ -24,10 +24,16 @@
 
     # always loaded
     'data': [
+        'security/real_estate_role.xml',
         'security/ir.model.access.csv',
         'views/estate_property_views.xml',
         'views/estate_property_type_views.xml',
+        'views/estate_property_tag_views.xml',
+        'views/estate_property_offer_views.xml',
         'views/real_estate_menu_views.xml',
+        'report/ir_report_template.xml',
+        'report/ir_report_action.xml',
+ 
         # 'views/templates.xml',
     ],
     # only loaded in demonstration mode
--- real_estate/models/estate_property.py
+++ real_estate/models/estate_property.py
@@ -1,11 +1,20 @@
-from odoo import models, fields
+from odoo import models, fields, api
+from odoo.exceptions import UserError
+from odoo.modules.module import get_module_resource
+import base64
+
 
 
 class EstateProperty(models.Model):
     _name = 'estate.property'
     _description = 'Estate Property'
 
-    name = fields.Char(string='Name')
+    # @api.model
+    # def _default_image(self):
+    #     image_path = get_module_resource('lunch', 'static/img', 'people.png')
+    #     return base64.b64encode(open(image_path, 'rb').read())
+
+    name = fields.Char(string='Name', required=True )
     description = fields.Text(string='Description')
     postcode = fields.Char(string='Postcode')
     date_availability = fields.Date(string='Date Availability')
@@ -24,6 +33,50 @@ class EstateProperty(models.Model):
                                                     ('w', 'W'),])
     estate_property_type_id = fields.Many2one(comodel_name='estate.property.type',
                                                 string='Property Type')
+    tag_ids = fields.Many2many(comodel_name='estate.property.tag', string='Tags')
+    total_area = fields.Integer(string='Total Area', compute="_compute_total_area")
+    state = fields.Selection(string='Status', selection=[('new', 'New'), 
+                                                         ('offer_received', 'Offer Received'),
+                                                         ('offer_accepted', 'Offer Accepted'),
+                                                         ('sold', 'Sold'),
+                                                         ('cancel', 'Cancel'),],
+                                                         default='new')
+    offer_ids = fields.One2many(comodel_name='estate.property.offer', inverse_name='property_id', string='Offers')
+    image_1920 = fields.Image()
+    
+    
+    @api.depends('living_area', 'garden_area')
+    def _compute_total_area(self):
+        for prop in self:
+            prop.total_area = prop.living_area + prop.garden_area
+    
+    @api.onchange('garage')
+    def onchange_garage(self):
+        for prop in self:
+            if prop.garage:
+                prop.garden=True
+            else:
+                prop.garden=False
+    
+    @api.constrains('garden','garden_area')
+    def _check_garden(self):
+        for prop in self:
+            if prop.garden_area and not prop.garden:
+                raise UserError('Anda tidak bisa mempunyai daerah kebun jika tidak punya kebun')
+            
+    def action_sold(self):
+        for prop in self:
+            prop.write({'state':'sold'})
+
+    def action_cancelled(self):
+        for prop in self:
+            prop.write({'state':'cancelled'})
+            
+    
+
+
+    
+    
     
     
     
